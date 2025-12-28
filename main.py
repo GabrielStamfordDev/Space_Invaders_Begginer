e026466b233e95f39e85df218c6c8b784eb34407
Gabriel Stamford <gabrielstamford21@hotmail.com>
2025-12-28 09:18:23 -0300

    create ship and it's movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
 
 # BG_COLOR = (230, 230, 230)
 
@@ -9,16 +10,47 @@ class AlienInvasion():
         pygame.init()
         self.setting = Settings()
         self.screen = pygame.display.set_mode((self.setting.screen_width,self.setting.screen_higth))
+        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        #self.setting.screen_height = self.screen.get_rect().height
+        #self.setting.screen_width = self.screen.get_rect().width
         self.clock  = pygame.time.Clock()
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
         # self.bg_color = (230,230,230)
+    
+    def _check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+                sys.exit()
+                    
+    def _check_keyup_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _check_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    def _update_screen(self):
+        self.screen.fill(self.setting.bg_color)
+        self.ship.blit_ship()
+        pygame.display.flip()
+
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.setting.bg_color)
-            pygame.display.flip()
+            self._check_event()
+            self._update_screen()
+            self.ship.update()
             self.clock.tick(60)
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -2,4 +2,7 @@ class Settings():
     def __init__(self):
         self.bg_color = (230,230,230)
         self.screen_width = 1200
-        self.screen_higth = 800
+        self.screen_higth = 700
+        #self.screen_width = 0
+        #self.screen_height = 0
+        self.ship_speed = 1.5
